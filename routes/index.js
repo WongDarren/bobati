6649e459a95c1bd946bfc390759bc2834d0a1803
WongDarren <wongdarren1997@gmail.com>
2019-10-26 01:55:34 -0700

    Initialize theme

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Boba Ti' });
+  res.render('index', { title: 'Boba Tí' });
 });
 
 module.exports = router;
